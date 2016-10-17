a3ba317035215ab1b430538a169f75069f9b8996
CryptCoderr <hermansamra1@gmail.com>
2016-10-17 02:56:12 -0700

    Add files via upload

---
+++ piEye_v1.py
@@ -0,0 +1,44 @@
+import RPi.GPIO as GPIO
+import smtplib
+import datetime
+
+   
+TO_EMAIL = "receiver email goes here"
+FROM_EMAIL = "sender email goes here"
+FROM_PASSWORD = "sender password goes here"
+SUBJECT = "INTRUDER!!
+
+# PIR GPIO set up
+PIR_PIN = 7 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIR_PIN, GPIO.IN,)    
+
+# infinite loop -> always running
+while True:
+    time.sleep(0.1)
+    previous_state = current_state
+	current_state = GPIO.input(PIR_PIN)
+	# check if current state has changed
+	if current_state != previous_state:
+	    if current_state:
+			
+			timestamp = datetime.now().strftime("%Y-%m-%d_%H.%M.%S") 
+    
+	        # set up gmail 
+	        server = smtplib.SMTP(FROM_EMAIL, 587)         
+	        server.starttls                                
+	        server.login(FROM_EMAIL, FROM_PASSWORD)
+			
+			BODY = "\r\n".join([
+            "TO: %s" % TO_EMAIL,
+            "From: %s" % FROM_EMAIL,
+            "Subject: %s" % SUBJECT,
+            "",
+            "Intruder has been spotted at %s" % timestamp
+            ])
+	
+	        # send gmail message 
+	        server.sendmail(FROM_EMAIL, TO_EMAIL, BODY)
+		    server.quit
+    	
---
+++ piEye_v3.py
@@ -0,0 +1,70 @@
+import RPi.GPIO as GPIO
+import gpiozero
+import smtplib
+import datetime
+import dropbox
+
+   
+TO_EMAIL = "receiver email goes here"
+FROM_EMAIL = "sender email goes here"
+FROM_PASSWORD = "sender password goes here"
+SUBJECT = "INTRUDER!!"
+DROPBOX_TOKEN = "dropbox token goes here"
+
+# PIR GPIO set up
+PIR_PIN = 7 
+LED_PIN = 8
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIR_PIN, GPIO.IN,)
+GPIO.setup(LED_PIN, GPIO.OUT)  
+
+# infinite loop -> always running
+while True:
+    time.sleep(0.1)
+    previous_state = current_state
+	current_state = GPIO.input(PIR_PIN)
+	# check if current state has changed
+	if current_state != previous_state:
+	    if current_state:
+			
+			# set up time and video file info
+			timestamp = datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
+            videoname = timestamp + ".mp4"			
+			filename = "/home/pi/" + videoname
+			
+			# take 10 second video
+			camera.start_recording(filename)
+			# flash LED
+			for i in range(6):
+			    GPIO.output(LED_PIN,1)
+				sleep(1)
+				GPIO.output(LED_PIN,0)
+				sleep(1)
+			camera.stop_recording()
+			
+			# upload video to Dropbox
+			vid = open(filename)
+			dropB = dropbox.Dropbox(DROPBOX_TOKEN)
+			dropB.files_upload(vid, videoname)
+			vid.close()
+    
+	        # set up gmail 
+	        server = smtplib.SMTP(FROM_EMAIL, 587)         
+	        server.starttls                                
+	        server.login(FROM_EMAIL, FROM_PASSWORD)
+			
+			BODY = "\r\n".join([
+            "TO: %s" % TO_EMAIL,
+            "From: %s" % FROM_EMAIL,
+            "Subject: %s" % SUBJECT,
+            "",
+            "Intruder has been spotted at %s" % timestamp,
+			"",
+			"Please see Dropbox Account for video"
+            ])
+	
+	        # send gmail message 
+	        server.sendmail(FROM_EMAIL, TO_EMAIL, BODY)
+		    server.quit
+    	
